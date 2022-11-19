ac5251d0cb422cfc6cad9bc4768e0edc9895dcbb
ChokDee-Sam <chokdee.sam@hotmail.com>
2022-11-18 22:33:54 -1000

    Commit 20 - Developer Skills (Problem 2)

--- 03-Developer-Skills/starter/exercice/exercice.js
+++ 03-Developer-Skills/starter/exercice/exercice.js
@@ -36,3 +36,39 @@ const calcTempAmplitude = function (temp) {
 const amplitude = calcTempAmplitude(temperatures);
 console.log(amplitude);
 
+// Problem 2
+
+const calcTempAmplitudeNew = function (temp1, temp2) {
+    console.log("--------------- DEUXIEME PROBLEME À REGLER");
+
+    const allTemps = temp1.concat(temp2);
+    console.log(allTemps);
+
+    lowestTemp = allTemps[0];
+    highestTemps = allTemps[0];
+
+    for (let i = 0; i < allTemps.length; i++) {
+        const curTemp = allTemps[i];
+        if (typeof curTemp !== "number") continue;
+
+        console.log("______ boucle______");
+        console.log(`LowestTemp = ${lowestTemp}`);
+        console.log(`HighestTemp = ${highestTemps}`);
+        console.log(`CurTemp = ${curTemp}`);
+
+        if (curTemp < lowestTemp) {
+            lowestTemp = curTemp;
+        }
+        if (curTemp > highestTemps) {
+            highestTemps = curTemp;
+        }
+    }
+    console.log(`_______ Résultat final 2`);
+    console.log(lowestTemp);
+    console.log(highestTemps);
+
+    return highestTemps - lowestTemp;
+};
+
+const amplitudeNew = calcTempAmplitudeNew([7, 5, 1], [9, -1, 5]);
+console.log(amplitudeNew);
