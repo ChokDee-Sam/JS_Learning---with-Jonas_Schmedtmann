ccca1eef28705dec9aad79bd034286cf53e57287
ChokDee-Sam <chokdee.sam@hotmail.com>
2022-11-20 23:11:39 -1100

    Commit 26 - DOM (Click Events)

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,10 +1,14 @@
 'use strict';
 
+// -------------------------------------------------------
+// --------- Selecting and Manipulating Elements ---------
+// -------------------------------------------------------
+
 // Pointer et afficher un contenu existant
 console.log(document.querySelector('.message').textContent);
 
 // Modifier un contenu
-document.querySelector('.message').textContent = '🍕 Correct Number';
+// document.querySelector('.message').textContent = '🍕 Correct Number';
 
 // Afficher le contenu modifié
 console.log(document.querySelector('.message').textContent);
@@ -14,5 +18,18 @@ document.querySelector('.number').textContent = '13';
 document.querySelector('.score').textContent = '10';
 
 // Dans un Input, on utilise .value
-document.querySelector('.guess').value = '23';
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = '0';
+// console.log(document.querySelector('.guess').value);
+
+//
+// --------------------------------
+// --------- Click Events ---------
+// --------------------------------
+
+// Ici, on crée une fonction localement, qui sera call quand il y aura l'event
+// Celle ci créera une variable contenant l'input
+// Et cet input passera de String à Number
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+});
