59f6fafa3be31ea5610b57e87b8b29973f8f789f
ChokDee-Sam <chokdee.sam@hotmail.com>
2022-11-20 22:25:18 -1100

    Commit 25 - DOM (Selecting and Manipulating elements)

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// Pointer et afficher un contenu existant
+console.log(document.querySelector('.message').textContent);
+
+// Modifier un contenu
+document.querySelector('.message').textContent = '🍕 Correct Number';
+
+// Afficher le contenu modifié
+console.log(document.querySelector('.message').textContent);
+
+// Pointer et afficher un contenu existant
+document.querySelector('.number').textContent = '13';
+document.querySelector('.score').textContent = '10';
+
+// Dans un Input, on utilise .value
+document.querySelector('.guess').value = '23';
+console.log(document.querySelector('.guess').value);
