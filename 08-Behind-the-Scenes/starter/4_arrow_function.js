ab3dfd015054f0e012d1198d10ceae20e152da2b
ChokDee-Sam <chokdee.sam@hotmail.com>
2022-12-08 14:02:48 +0700

    Commit 42 - JS Behind the Scenes (Regular Functions vs. Arrow Functions - Improved Part.1 )

--- 08-Behind-the-Scenes/starter/4_arrow_function.js
+++ 08-Behind-the-Scenes/starter/4_arrow_function.js
@@ -1,5 +1,80 @@
 'use strict';
 
+// --------------------------------------------------------------------------
+// Le piège de la Fonction Flêchée + this : valeur inexistante
+// --------------------------------------------------------------------------
+
+// --- On sait que, lorsqu'on crée une fonction flêchée, le 'this' est inexistant dans sa Scope
+// ------ De ce fait, il remonte chercher l'information du 'this' chez le parent (ici Global Scope)
+
+const piege_1 = {
+  name: 'Sam',
+  year: 1983,
+
+  calcAge: function () {
+    console.log(this);
+    console.log(2022 - this.year);
+  },
+
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.name}`); // n'affiche pas le 'this.name' !!
+  },
+};
+
+piege_1.greet();
+console.log(this.name); // rien ne s'affiche dans la console (juste un espace vide)
+
+// --------------------------------------------------------------------------
+// Le piège de la Fonction Flêchée + this + 'var'
+// --------------------------------------------------------------------------
+
+// --- En activant cette ligne
+// ------ Un 'this' d'une fonction flêchée trouvera cette info Parent et l'utilisera
+// ------ Ce qui créera un bug chez nous, car on ne voulait pas cette Scope
+// ------ Nous savons qu'une 'var' créera sa propriété sur l'Objet Global (Window)
+// var name = 'Sam';
+
+const piege_2 = {
+  name: 'Sam',
+  year: 1983,
+
+  calcAge: function () {
+    console.log(this);
+    console.log(2022 - this.year);
+  },
+
+  greet: () => console.log(`Hey ${this.name}`), // n'affiche pas le 'this.name' !!
+  // SAUF si on active la variable ligne 33
+};
+
+piege_2.greet();
+console.log(this.name); // rien ne s'affiche dans la console (juste un espace vide)
+// SAUF si on active la variable ligne 33
+
+// --------------------------------------------------------------------------
+// La Solution pour éviter ces pièges
+// --------------------------------------------------------------------------
+
+// --- Ne pas utiliser de fonction flêchée en tant que Method
+// ------ Utiliser une fonction régulière
+
+const piege_3 = {
+  name: 'Joe',
+  year: 42,
+
+  calcAge: function () {
+    console.log(this);
+    console.log(2022 - this.year);
+  },
+
+  greet: function () {
+    console.log(this);
+    console.log(`Hey ${this.name}`);
+  },
+};
+
+piege_3.greet();
 
 // --------------------------------------------------------------------------
 // --------------------------------------------------------------------------
@@ -121,3 +196,29 @@ troisiemeExemple.calcAge();
 // --------------------------------------------------------------------------
 //
 //
+// --------------------------------------------------------------------------
+// Bonnes pratiques
+// --------------------------------------------------------------------------
+
+// Ne JAMAIS utiliser une fonction flêchée (Method) dans un objet
+// En d'autre termes : pas de fonction flêchée pour créer une Method !!
+
+// --- Et pour éviter les erreurs : ne pas utiliser de this dans une Method particulière
+// ------ Parce que si on a cette règle de ne jamais utiliser de fonction flêchée en tant que
+// ------ Method, alors on aura pas à penser à quel type de fonction utiliser
+
+// --- Toujours utiliser une fonction d'expression normale
+// ------ pour éviter de produire ce genre d'erreur
+
+// --- Passer de ça : fonction flêchée
+//                greet: () => console.log(`Hey ${this.name}`)
+
+// À ça : fonction régulière
+//                greet: function () {
+//                  console.log(`Hey ${this.name}`)
+//                  }
+
+// --- Ne pas utiliser de 'var'
+
+// Quelle que soit la fonction (flêchée, )
+// 5:48 secondes : à complèter...
